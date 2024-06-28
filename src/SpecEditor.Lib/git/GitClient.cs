6657cb1857154e861a256c64ef93bfe1e3eaf775
imaun <imun22@gmail.com>
2024-06-28 22:43:54 +0200

    add GitClient

--- src/SpecEditor.Lib/git/GitClient.cs
+++ src/SpecEditor.Lib/git/GitClient.cs
@@ -1,11 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using LibGit2Sharp;
 
 namespace SpecEditor.Lib;
 
 public class GitClient
 {
+
+
+    public void git_clone(string url, string localPath, string branchName)
+    {
+        try
+        {
+            Repository.Clone(url, localPath, new CloneOptions {
+                BranchName = branchName,
+                Checkout = true
+            });
+        }
+        catch(Exception ex)
+        {
+            //TODO: log the exception
+            throw;
+        }
+    }
+
 }
