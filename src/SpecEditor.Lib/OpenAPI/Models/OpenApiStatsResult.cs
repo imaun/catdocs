51e451b222ec29bd802e7b972b08a29657003a7c
imaun <imun22@gmail.com>
2024-06-30 00:02:20 +0200

    add ToString() method for OpenApiStatsResult

--- src/SpecEditor.Lib/OpenAPI/Models/OpenApiStatsResult.cs
+++ src/SpecEditor.Lib/OpenAPI/Models/OpenApiStatsResult.cs
@@ -1,7 +1,26 @@
-﻿namespace SpecEditor.Lib.OpenAPI;
+﻿using System.Text;
+
+namespace SpecEditor.Lib.OpenAPI;
 
 public record OpenApiStatsResult(
     int PathItemsCount, int OperationsCount, int ParametersCount,
     int RequestBodyCount, int ResponsesCount, int LinkesCount,
     int CallbacksCount, int SchemasCount
-    );
+    )
+{
+
+    public override string ToString()
+    {
+        var sb = new StringBuilder();
+        sb.AppendLine($"PathItems : {PathItemsCount}")
+            .AppendLine($"Operations : {OperationsCount}")
+            .AppendLine($"Parameters : {ParametersCount}")
+            .AppendLine($"RequestBodies : {RequestBodyCount}")
+            .AppendLine($"Responses : {ResponsesCount}")
+            .AppendLine($"Links : {LinkesCount}")
+            .AppendLine($"Callbacks : {CallbacksCount}")
+            .AppendLine($"Schemas : {SchemasCount}");
+
+        return sb.ToString();
+    }
+}
