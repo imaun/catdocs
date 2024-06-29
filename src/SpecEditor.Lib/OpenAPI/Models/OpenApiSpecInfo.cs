5d320b1abecca625c54996604558476339c1a469
imaun <imun22@gmail.com>
2024-06-30 00:46:03 +0200

    add OpenApiSpecInfo

---
+++ src/SpecEditor.Lib/OpenAPI/Models/OpenApiSpecInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace SpecEditor.Lib.OpenAPI;
+
+public record OpenApiSpecInfo(
+    string InputFile,
+    bool HasErrors,
+    string Version,
+    string Format,
+    bool IsJson,
+    bool IsYaml,
+    List<string> Errors
+    );
