91ff2d78db47860ff44d2b383558af6c1b469f96
imaun <imun22@gmail.com>
2024-06-30 18:47:18 +0200

    add SpecLogger to log exceptions

---
+++ src/SpecEditor.Lib/OpenAPI/SpecLogger.cs
@@ -0,0 +1,30 @@
+﻿namespace SpecEditor.Lib.OpenAPI;
+
+public static class SpecLogger
+{
+    
+    public static string Filename { get; private set; }
+
+    public static void SetLogFilename(string filePath)
+    {
+        var fileName = Path.GetFileNameWithoutExtension(filePath);
+        var now = DateTime.Now;
+        Filename = $"{fileName}-{now.Date.ToShortDateString()}-{now.ToShortTimeString()}";
+    }
+
+    public static void Log(string log)
+    {
+        if (string.IsNullOrWhiteSpace(log))
+        {
+            return;
+        }
+
+        var log_item = $"{DateTime.Now.ToLongDateString()}: {log}";
+        var fs = new FileStream(
+            Filename, FileMode.Append, FileAccess.Write, FileShare.Read);
+        using var stream_writer = new StreamWriter(fs);
+        stream_writer.WriteLine(log_item);
+        stream_writer.Flush();
+        stream_writer.Close();
+    }
+}
