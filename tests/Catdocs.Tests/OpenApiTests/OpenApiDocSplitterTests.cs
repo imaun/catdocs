fa82edfdf583a9fd399bafc5c52faa0bd07bb0d8
imaun <imun22@gmail.com>
2024-07-05 22:20:12 +0200

    add unit test for GetRelativePath in OpenApiDocSplitter

--- tests/Catdocs.Tests/OpenApiTests/OpenApiDocSplitterTests.cs
+++ tests/Catdocs.Tests/OpenApiTests/OpenApiDocSplitterTests.cs
@@ -1,6 +1,22 @@
-﻿namespace Catdocs.Tests.OpenApiTests;
+﻿using Catdocs.Lib.OpenAPI.Internal;
+using Microsoft.OpenApi.Models;
+
+namespace Catdocs.Tests.OpenApiTests;
 
 public class OpenApiDocSplitterTests
 {
     
+    [Theory]
+    [InlineData(@"C:\example\output\paths\get_by_id.yaml", @"C:\example\output", @"paths\get_by_id.yaml")]
+    [InlineData("~/example/output/paths/get_by_id.yaml", "~/example/output", @"paths\get_by_id.yaml")]
+    public void GetRelativePath_Should_Return_Correct_RelativePath(
+        string inputFilePath, string outputDir, string expectedRelativePath)
+    {
+        var document = new OpenApiDocument();
+        var splitter = new OpenApiDocSplitter(outputDir, document);
+
+        string actualRelativePath = splitter.GetRelativePath(inputFilePath);
+
+        Assert.Equal(expectedRelativePath, actualRelativePath);
+    }
 }
