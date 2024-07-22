bfdaa51f6eb0953add2916b8e80bc026a39a0322
imaun <imun22@gmail.com>
2024-07-22 23:25:59 +0200

    add Load_valid_open_api_doc_succeed with theory data

--- tests/Catdocs.Tests/OpenApiTests/OpenAPISpecParser_Load_Tests.cs
+++ tests/Catdocs.Tests/OpenApiTests/OpenAPISpecParser_Load_Tests.cs
@@ -7,13 +7,15 @@ namespace Catdocs.Tests.OpenApiTests;
 public class OpenAPISpecParser_Load_Tests
 {
     
-    [Fact]
-    public void load_valid_open_api_document_should_succeed()
+    [Theory]
+    [InlineData(OpenApiSpecVersion.OpenApi3_0, OpenApiFormat.Yaml, "valid_simple_oas.yaml")]
+    [InlineData(OpenApiSpecVersion.OpenApi3_0, OpenApiFormat.Json, "valid_simple_oas.json")]
+    public void load_valid_open_api_document_should_succeed(
+        OpenApiSpecVersion version, OpenApiFormat format, string filePath)
     {
-        var validFilePath = "valid_simple_oas.yaml";
-
+        
         var parser = new OpenAPISpecParser(
-            TestDataProvider.GetTestDataFilePath(validFilePath)
+            TestDataProvider.GetTestDataFilePath(filePath), version, format
             );
 
         var info = parser.Load();
