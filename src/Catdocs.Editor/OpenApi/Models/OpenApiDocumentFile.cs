dabaa99c32a556071517f54b2d0775ae7ff31ad7
imaun <imun22@gmail.com>
2024-08-10 01:29:44 +0200

    add load document for OpenApiDocumentFile model

--- src/Catdocs.Editor/OpenApi/Models/OpenApiDocumentFile.cs
+++ src/Catdocs.Editor/OpenApi/Models/OpenApiDocumentFile.cs
@@ -1,15 +1,25 @@
+using System.Collections.Generic;
 using System.IO;
+using Catdocs.Lib.OpenAPI;
+using Microsoft.OpenApi.Models;
 
 namespace Catdocs.Editor.OpenApi;
 
 public class OpenApiDocumentFile
 {
-
-    public OpenApiDocumentFile(FileInfo fileInfo)
+    
+    public OpenApiDocumentFile(FileInfo fileInfo, bool loadDocument = false)
     {
         FilePath = fileInfo.FullName;
         FileName = fileInfo.Name;
         Extension = fileInfo.Extension;
+        Errors = new List<string>();
+        
+        if (loadDocument)
+        {
+            Info = Load();
+            Document = Info.Document;
+        }
     }
     
     public string FilePath { get; }
@@ -17,4 +27,16 @@ public class OpenApiDocumentFile
     public string FileName { get; }
     
     public string Extension { get; }
+    
+    public OpenApiDocument? Document { get; }
+    
+    public OpenApiSpecInfo? Info { get; }
+    
+    public IReadOnlyCollection<string> Errors { get; }
+
+    private OpenApiSpecInfo Load()
+    {
+        var parser = new OpenAPISpecParser(FilePath);
+        return parser.Load();
+    }
 }
