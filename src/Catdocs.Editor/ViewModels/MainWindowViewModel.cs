2f1458b6ad8387ef4893a93ba8b1a7472d4cbfe4
imaun <imun22@gmail.com>
2024-08-10 00:55:22 +0200

    add Catdocs.Editor project

---
+++ src/Catdocs.Editor/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,5 @@
+﻿namespace Catdocs.Editor.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+}
---
+++ src/Catdocs.Editor/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Catdocs.Editor.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
